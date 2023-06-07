0a8d115297b05649271c8ae2462c0165b61d0e19
christopher <christopherstefano1@gmail.com>
2023-06-07 13:07:59 -0300

    initial

---
+++ src/main/java/com/erp/autenticador/controller/UsuarioController.java
@@ -0,0 +1,14 @@
+package com.erp.autenticador.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    public ResponseEntity criarusuario() {
+        return null;
+    }
+}
---
+++ src/test/java/com/erp/autenticador/AutenticadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.autenticador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
